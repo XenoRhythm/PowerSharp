7beefa14c99dd02588776503fb4cc3ff76314207
xenorhythm <foo@bar.local>
2023-10-09 02:13:32 +0600

    Add project files.

---
+++ powersharp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Management.Automation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+//todo
+//etw patching
+//amsi bypass
+
+namespace powersharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                if (args[0] == "-b64")
+                {
+                    if (!string.IsNullOrEmpty(args[1]))
+                    {
+                        var bytes = Convert.FromBase64String(args[1]);
+                        var text = System.Text.Encoding.UTF8.GetString(bytes);
+                        PSExec(text);
+                    }
+                }
+                else
+                {
+                    PSExec(string.Join(" ", args));
+                }
+            }
+            //Console.ReadKey();
+            return;
+        }
+
+        static void PSExec(string cmd)
+        {
+            cmd = GetResource() + Environment.NewLine + cmd;
+
+            PowerShell ps = PowerShell.Create();
+            Collection<PSObject> results = ps.AddScript(cmd).Invoke();
+
+            if (ps.HadErrors)
+            {
+                foreach (var error in ps.Streams.Error)
+                {
+                    Console.WriteLine("Error: " + error.ToString());
+                }
+            }
+            else
+            {
+                foreach (PSObject result in results)
+                {
+                    Console.WriteLine(result.ToString());
+                }
+            }
+        }
+        static string GetResource()
+        {
+            string resName = null;
+            if ((resName = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceNames().FirstOrDefault()) != null)
+            {
+                return new StreamReader(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resName)).ReadToEnd();
+            }
+            return null;
+        }
+    }
+}
+
+
---
+++ powersharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("powersharp")]
+[assembly: AssemblyDescription("psh host app")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("x3n-sec")]
+[assembly: AssemblyProduct("powersharp")]
+[assembly: AssemblyCopyright("x3n-sec ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b7ade00b-245d-4f94-8a71-d3048e1adcfe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
